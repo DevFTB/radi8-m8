20c7397b41b9f26aaf0f3a52be8145795be075e4
Aditha Doratiyawa <indegamedev@gmail.com>
2021-10-02 20:47:54 +1000

    added claw attack animation

--- game/Scripts/Enemy/ClawMutation.gd
+++ game/Scripts/Enemy/ClawMutation.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 
-export (float) var attackSpeed = 5
+export (float) var attackSpeed = 2
 export (float) var amountOfSwipes = 10;
 
 var timer: float = 0.0;
@@ -9,7 +9,8 @@ var swipeCount: int = 0;
 var attacking: bool = false;
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimatedSprite.play()
+	$AnimatedSprite.play("equip")
+
 	pass # Replace with function body.
 
 func _process(delta):
@@ -19,13 +20,17 @@ func _process(delta):
 		timer = 0.0;
 
 func attack():
+	$AnimatedSprite.set_speed_scale(attackSpeed);
 	attacking = true;
 
 func fire():
 	if(swipeCount < amountOfSwipes):
 		swipeCount += 1
+		$AnimatedSprite.frame = 0;
+		$AnimatedSprite.play("attack")
 		print(swipeCount)
 		print("swipe")
 	else:
+		$AnimatedSprite.set_speed_scale(1);
 		attacking = false; 
 		swipeCount = 0
