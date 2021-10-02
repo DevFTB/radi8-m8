2852328a7de16a78964f5145a784fb4492d73518
Saurabh <jhanjeesaurabh@gmail.com>
2021-10-02 19:36:48 +1000

    Added damage, hurtboxes and hitboxes, enemy death and fixed merge

--- game/scripts/Enemy/Enemy.gd
+++ game/scripts/Enemy/Enemy.gd
@@ -1,12 +1,18 @@
 extends KinematicBody2D
 
+signal health_changed
+signal no_health
+
 export(float) var speed = 40
 export(float) var attackPeriod = 3
-export(float) var mutationPeriod = 10;
+export(float) var mutationPeriod = 10
+export(int) var max_health = 5
 export (NodePath) var playerPath
 
-onready var navigation = get_node("/root/Level/Map/Navigation2D")
+onready var navigation = get_parent().room
 onready var player = get_node(playerPath)
+
+onready var health = max_health setget set_health
 
 var path = null
 var velocity = Vector2.ZERO
@@ -18,6 +24,7 @@ var mutations = []
 var activeMutations: Array
 var inactiveMutations: Array
 # Called when the node enters the scene tree for the first time.
+
 func _ready():
 	print(get_children())
 	mutations = $Mutations.get_children()
@@ -26,7 +33,7 @@ func _ready():
 	inactiveMutations.append_array(mutations.slice(1, mutations.size()))
 	for mut in inactiveMutations:
 		print(mut)
-		get_node("Mutations").remove_child(mut)
+		$Mutations.remove_child(mut)
 	
 func _process(delta):
 	attackTimer += delta
@@ -41,7 +48,8 @@ func _process(delta):
 		mutate();
 
 func _physics_process(delta):
-	if(player and navigation): 
+	navigation = get_parent().room
+	if(player and navigation):
 		genPath()
 		navigate()
 	move()
@@ -60,7 +68,7 @@ func mutate():
 		print("mutate")
 		var index = randi() % inactiveMutations.size()
 		var newMutation = inactiveMutations[index]
-		get_node("Mutations").add_child(newMutation)
+		$Mutations.add_child(newMutation)
 		activeMutations.append(newMutation)
 		inactiveMutations.remove(index)
 
@@ -77,3 +85,19 @@ func navigate():
 
 func move():
 	velocity = move_and_slide(velocity)
+	
+func take_damage(value):
+	set_health(health - value)
+
+func set_health(value):
+	health = clamp(value, 0, max_health)
+	emit_signal("health_changed")
+	if (health == 0):
+		emit_signal("no_health")
+		queue_free()
+
+func _on_Hurtbox_area_entered(area):
+	take_damage(area.damage)
+
+func die():
+	queue_free()
